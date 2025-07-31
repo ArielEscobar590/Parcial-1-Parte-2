03ce2a038ec8c7f7e5d9e17d145911f741d1e293
German Escobar <germanjosearielescobarescobar@gmail.com>
2025-07-30 18:30:09 -0600

    Parcial2

---
+++ Parte2.py
@@ -0,0 +1,32 @@
+while (True):
+    try:
+        print("\n--- MENU ---")
+        print("1. Calcular MCD de dos números")
+        print("2. Crear una cadena repetidad N veces")
+        print("3. Conteo de veces que aparece una letra en una cadena")
+        print("4. Convertir un número decimal a Binario")
+        print("5. Conteo de dígitos en un número")
+        print("6. Salir")
+        op = int(input("Ingrese una opción para continuar"))
+
+        if op == 1:
+            def CalculoMCD(num1,num2):
+
+        elif op == 2:
+            def Cadena():
+
+
+        elif op == 3:
+            def Conteoletra(num):
+
+
+        elif op == 4:
+            def Convertir(num):
+
+        elif op == 5:
+            def Conteodigi():
+
+        elif op == 6:
+            break
+    except:
+        print("Ha ocurrido un error")
